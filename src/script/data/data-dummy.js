fd59e911dd0b2ea6da8a070b8a83d74d1dc55509
Aliffaturahman <100842759+Aliffaturahman@users.noreply.github.com>
2026-08-21 14:11:58 +0700

    Update note titles and content for personalization

--- src/script/data/data-dummy.js
+++ src/script/data/data-dummy.js
@@ -1,7 +1,7 @@
 const notesData = [
   {
     id: 'notes-jT-jjsyz61J8XKiI',
-    title: 'Welcome to Notes, Dimas!',
+    title: 'Welcome to Notes, Alif!',
     body: 'Welcome to Notes! This is your first note. You can archive it, delete it, or create new ones.',
     createdAt: '2022-07-28T10:03:12.594Z',
     archived: false,
